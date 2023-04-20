0aa7d184673503110ae2fa0277a3d4bdbeb642dc
ce2022069 <130224047+ce2022069@users.noreply.github.com>
2023-04-20 15:21:05 +0530

    Add files via upload

---
+++ 2_5.cpp
@@ -0,0 +1,35 @@
+/*Write a program to print global and local 
+values of a variable using scope resolution 
+operator.*/
+
+#include <iostream>
+#include <conio.h> 
+#include <string>
+ using namespace std; 
+  
+ // Define a global variable 
+ string global_var = "I am a global variable"; 
+  
+ void print_variables() { 
+     // Define a local variable 
+     string local_var = "I am a local variable"; 
+      
+     // Print the values of the variables 
+     cout << "Global variable: " << global_var << endl; 
+     cout << "Local variable: " << local_var << endl; 
+ } 
+  
+ int main() { 
+     system("cls"); //clrscr(); 
+     
+     print_variables(); 
+     getch(); 
+     return 0; 
+ }
+ 
+ /*
+ Output :
+ Global variable: I am a global variable
+Local variable: I am a local variable
+
+*/
